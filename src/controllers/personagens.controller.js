b49480bb6e604ac107c52d9040fe5170dd5c8bc0
leotinoco7 <leofptinoco7@gmail.com>
2022-04-19 20:54:27 -0300

    a

--- src/controllers/personagens.controller.js
+++ src/controllers/personagens.controller.js
@@ -15,36 +15,56 @@ const findPersonagemByIdController = (req, res) => {
     res.send(chosenPersonagem);
 }
 
-const addPersonagemController = (req, res) => {
-    let retorno;
 
-    if (req.body.id) {
-        retorno = personagensService.addPersonagemService(req.body);
-    } else {
-        res.send({ erro: 'falta id'});
-    } 
-    if (retorno == 'ok') {
-        res.send ({ message: 'cadastrado com sucesso'});
-    } else {
-        res.send('erro')
-    }
+const addPersonagemController = (req, res) => {
+    const personagem = req.body;
+
+    if (
+        !personagem ||
+        !personagem.nome ||
+        !personagem.descricao ||
+        !personagem.habilidade ||
+        !personagem.foto
+      ) {
+        return res.status(400).send(
+          { message: "faltam dados"}
+        );
+      }
+      const newPersonagem = personagensService.addPersonagemService(personagem);
+      res.send(newPersonagem);
 }
 
 const updatePersonagemController = (req, res) => {
     const idParam = +req.params.id;
     const personagemEdit = req.body;
-    const updatedPersonagem = personagensService.updatePersonagemService(
-        idParam,
-        personagemEdit,
-    );
+
+    if (!personagemEdit || !personagemEdit.sabor || !personagemEdit.descricao || !personagemEdit.foto || !personagemEdit.preco) {
+        return res.status(400).send({ message: "faltam dados" });
+      }
+    
+      const chosenPersonagem = personagensService.findPersonagemByIdService(idParam);
+    
+      if (!chosenPersonagem) {
+        return res.status(404).send({ message: "not found" })
+      }
+      
     res.send(updatedPersonagem);
 };
 
 const deletePersonagemController = (req, res) => {
     const idParam = req.params.id;
+
+    const chosenPersonagem = personagensService.findPersonagensByIdService(idParam);
+
+    if (!chosenPersonagens) {
+    return res.status(404).send({ message: "not found" })
+  }
+
     personagensService.deletePersonagemService(idParam);
     res.send({ message: 'deletado com sucesso!' });
 };
+
+
 module.exports = {
     homePersonagemController,
     findPersonagensController,
